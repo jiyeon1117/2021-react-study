97f3e93ef0e762bf2d53e189325cb89322059150
jiyeon1117 <s2019w24@e-mirim.hs.kr>
2021-07-21 14:18:44 +0900

    210721 - component 사용

--- react-class/react-ex04/src/App.js
+++ react-class/react-ex04/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
-  return <MyComponent>리액트 컴포넌트 사이</MyComponent>
+  return (
+    <MyComponent name="react" favoriteNumber = {1}>
+      리액트 컴포넌트 사이
+    </MyComponent>
+  );
 }
 
 export default App;
--- react-class/react-ex04/src/components/MyComponent.js
+++ react-class/react-ex04/src/components/MyComponent.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = props => {
-  return (
+class MyComponent extends Component{
+  static defaultProps = {
+    name : '이름'
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+  };
+
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
     <div>
-      안녕하세요, 제 이름은 {props.name}입니다. <br/>
-      children값은 {props.children}입니다.
+      안녕하세요, 제 이름은 {name}입니다. <br/>
+      children값은 {children}입니다.
+      <br/>
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
-  );
-}
-
-MyComponent.defaultProps = {
-  name: 'Jiyeon'
+    );
+  }
 }
 
 export default MyComponent;
